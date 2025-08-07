3151487830dd2a124ea742198b948ba4e8bb54df
palak-1505 <palakgang1505@gmail.com>
2025-08-07 14:48:27 +0530

    add successfully

--- ContextLocal/src/App.jsx
+++ ContextLocal/src/App.jsx
@@ -1,10 +1,11 @@
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TodoContextProvider } from "./context/TodoContext";
+import TodoForm from "./components/Todoform";
 
 function App() {
 
-  const [todo ,setTodo]=useState([])
+  const [todos ,setTodo]=useState([])
 
   const addTodo = (todo)=>{
     setTodo((prev)=>{[{id:Date.now(),...todo},...prev]})
@@ -28,6 +29,18 @@ function App() {
     ))
   }
 
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length > 0){
+      setTodo(todos)
+    }
+  },[])
+
+  useEffect( () => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
+
   
   return (
 
@@ -36,7 +49,8 @@ function App() {
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        {/* Todo form goes here */}
+                        <TodoForm/> 
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
