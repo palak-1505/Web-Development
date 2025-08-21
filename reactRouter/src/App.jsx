e4affaf953885b4d2db121c6267706c4e3dd492a
palak-1505 <palakgang1505@gmail.com>
2025-08-21 19:34:56 +0530

    add

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header/Header"
 import Home from "./components/Home/Home"
 import Footer from "./components/Footer/Footer"
+import Layout from "./Layout"
 
 function App() {
   
   return (
     <>
-      
+      <Layout/>
     </>
   )
 }
