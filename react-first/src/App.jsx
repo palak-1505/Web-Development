7c419efde723c42f8169a8b3159acc278207ff4d
palak-1505 <palakgang1505@gmail.com>
2025-08-30 00:14:19 +0530

    done

--- react-first/src/App.jsx
+++ react-first/src/App.jsx
@@ -3,33 +3,45 @@ import Demo from "./demo";
 
 function App() {
 
-   let [count,setCount] = useState(0);
+  //  let [count,setCount] = useState(0);
 
-   let add = ()=>{
-    if(count>=20){
-      alert("Can't greater than 20");
-    }
-    else{
-      setCount(count+1);
-    }
-   }
+  //  let add = ()=>{
+  //   if(count>=20){
+  //     alert("Can't greater than 20");
+  //   }
+  //   else{
+  //     setCount(count+1);
+  //   }
+  //  }
 
-   let remove = ()=>{
-    if(count==0){
-      alert("Can't be negative number");
-    }else{
-    setCount(count-1);
-    }
-   }
+  //  let remove = ()=>{
+  //   if(count==0){
+  //     alert("Can't be negative number");
+  //   }else{
+  //   setCount(count-1);
+  //   }
+  //  }
+
+  const [Color , setColor] = useState("blue");
+
+
 
   return (
     <>
      
-      <h1>Counter</h1>
+      {/* <h1>Counter</h1>
       <h2>Count : {count}</h2>
       <button onClick={add}>Increase</button>
       <br />
-      <button onClick={remove}>Decrease</button>
+      <button onClick={remove}>Decrease</button> */}
+
+      <div className="flex justify">
+        <div>
+          <button>Green</button>
+          <button>Yellow</button>
+          <button>Red</button>
+        </div>
+      </div>
     </>
   )
 }
--- reactRouter/src/components/Header/Header.jsx
+++ reactRouter/src/components/Header/Header.jsx
@@ -30,7 +30,7 @@ function Header() {
                     </div>
 
                      <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                        className=" justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
                         id="mobile-menu-2"
                     >
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
